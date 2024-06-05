b0c1fdd90a49128af3c19c69e08ad047c70b8c5c
@Nagendra_Yaparala <yaparalanagendra@gmail.com>
2024-06-05 14:25:45 +0530

    change_in_home

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <div>
         <h1 className="text-red-500">Hello</h1>
         <h1 className="text-green">New Portfolio</h1>
+        <h1>NagendraYaprala</h1>
       </div>
     </main>
   );
